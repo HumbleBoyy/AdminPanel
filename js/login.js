bfe5b859ae9847eeb3fdb00b9ad4592b05576f60
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-12-14 00:26:57 +0500

    New Commit

--- js/login.js
+++ js/login.js
@@ -1,4 +1,6 @@
 let elLoginForm = document.querySelector(".login_form")
+let elLoginBtn = document.querySelector(".login_btn")
+
 
 elLoginForm.addEventListener("submit", (e)=> {
     e.preventDefault()
@@ -7,6 +9,20 @@ elLoginForm.addEventListener("submit", (e)=> {
         username: e.target.username.value,
         password: e.target.password.value
     }
-
-    console.log(data)
+    elLoginBtn.innerHTML = `<img class="scale-[1.2]" src="./images/loader.png" alt="loader" width="50" height="50">`
+    if(data.username === "Asadulloh" && data.password === "123"){
+        setTimeout(()=> {
+            elLoginBtn.innerHTML = `Войти`
+            location.pathname = "./products.html"
+        },1000)
+    }else{
+        setTimeout(()=> {
+            elLoginBtn.classList.add("redBg")
+            elLoginBtn.innerHTML = `Неправильный пароль`
+        },1000)
+        setTimeout(()=> {
+            elLoginBtn.classList.remove("redBg")
+            elLoginBtn.innerHTML = `Войти`
+        },3500)
+    }
 })
