aef7bfcbb9bbce3829d3630ccfc5b0664177875c
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-12-16 21:10:31 +0500

    New Commit

--- js/app.js
+++ js/app.js
@@ -30,5 +30,28 @@ elModalWrapper.addEventListener("click", (e)=> {
 //  Add Modal Open start
 function handleAddClickBtn(){
      elModalWrapper.classList.remove("scale-0")
+
+     elModalInnerWrapper.innerHTML = `
+     <form class="w-[915px] mx-auto">
+         <label class="inline-block w-[full] mb-[33px] flex justify-center">
+             <input type="file" class="hidden"/>
+             <img src="./images/imageD.png" alt="Just Image" width="500" height="200"/>
+         </label>
+     <div class="flex justify-between">
+        <div class="w-[49%]">
+           <label>
+           <span class="text-[20px] mb-[9px] text-[#898989] leading="32px" ">Категории</span>
+              <select class="w-full py-[15px] rounded-[5px] bg-slate-100 shadow-xl text-[25px] text-[#545454] outline-none">
+                <option value="0">Каркасные</option>
+                <option value="1">Надувные</option>
+             </select>
+           </label>
+        </div>
+         <div class="w-[49%]">
+        
+        </div>
+     </div>
+    </form>
+     `
 }
 //  Add Modal Open end
