7846eedc3704e3fef23c3db7cf6ac2b3cd067e67
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-12-17 10:28:01 +0500

    New Commit

--- js/app.js
+++ js/app.js
@@ -33,24 +33,58 @@ function handleAddClickBtn(){
 
      elModalInnerWrapper.innerHTML = `
      <form class="w-[915px] mx-auto">
-         <label class="inline-block w-[full] mb-[33px] flex justify-center">
-             <input type="file" class="hidden"/>
-             <img src="./images/imageD.png" alt="Just Image" width="500" height="200"/>
+         <label class="inline-block w-[full] mb-[10px] flex justify-center">
+             <input type="file" class="hidden" name="imageUrl"/>
+             <img src="./images/imageD.png" alt="Just Image" width="400" height="100"/>
          </label>
      <div class="flex justify-between">
         <div class="w-[49%]">
            <label>
            <span class="text-[20px] mb-[9px] text-[#898989] leading="32px" ">Категории</span>
-              <select class="w-full py-[15px] rounded-[5px] bg-slate-100 shadow-xl text-[25px] text-[#545454] outline-none">
+              <select name="category_id" class="w-full p-[12px] rounded-[5px] bg-slate-100 shadow-xl text-[25px] text-[#545454] outline-none">
                 <option value="0">Каркасные</option>
                 <option value="1">Надувные</option>
              </select>
            </label>
+           <label>
+           <span class="text-[20px] mb-[9px] text-[#898989] leading="32px" ">Стартая цена (сум) </span>
+              <input type="number" name="price" class="w-full p-[12px] rounded-[5px] bg-slate-100 shadow-xl text-[25px] text-[#545454] outline-none" placeholder="2500000"/>
+           </label>
+            <label>
+           <span class="text-[20px] mb-[9px] text-[#898989] leading="32px" ">Рамка</span>
+              <input type="text" name="frame" class="w-full p-[12px] rounded-[5px] bg-slate-100 shadow-xl text-[25px] text-[#545454] outline-none" placeholder="10"/>
+           </label>
+            <label>
+           <span class="text-[20px] mb-[9px] text-[#898989] leading="32px" ">Размер (м)</span>
+              <input type="text" name="size" class="w-full p-[12px] rounded-[5px] bg-slate-100 shadow-xl text-[25px] text-[#545454] outline-none" placeholder="Размер (м)"/>
+           </label>
         </div>
          <div class="w-[49%]">
-        
+           <label>
+           <span class="text-[20px] mb-[9px] text-[#898989] leading="32px" ">Статус</span>
+              <select name="recommendation" class="w-full p-[12px] rounded-[5px] bg-slate-100 shadow-xl text-[25px] text-[#545454] outline-none">
+                <option value="0">Рекомендуем</option>
+                <option value="1">Cкидка</option>
+                <option value="2">Нет в наличии</option>
+             </select>
+           </label>
+           <label>
+           <span class="text-[20px] mb-[9px] text-[#898989] leading="32px" ">Количество</span>
+              <input type="number" name="quantity" class="w-full p-[12px] rounded-[5px] bg-slate-100 shadow-xl text-[25px] text-[#545454] outline-none" placeholder="10"/>
+           </label>
+            <label>
+           <span class="text-[20px] mb-[9px] text-[#898989] leading="32px" ">Цена со скидкой (сум) </span>
+              <input type="text" name="discount_price" class="w-full p-[12px] rounded-[5px] bg-slate-100 shadow-xl text-[25px] text-[#545454] outline-none" placeholder="10"/>
+           </label>
+            <label>
+           <span class="text-[20px] mb-[9px] text-[#898989] leading="32px" ">Глубина(см)</span>
+              <input type="text" name="depth" class="w-full p-[12px] rounded-[5px] bg-slate-100 shadow-xl text-[25px] text-[#545454] outline-none" placeholder="Глубина(см)"/>
+           </label>
         </div>
      </div>
+      <div class="flex justify-center items-center mt-5">
+         <button onclick="handleAddSecondClickBtn()" class="w-[222px] h-[45px] bg-[#009398] text-white rounded-[30px] shadow-md shadow-slate-300">Добавить</button>
+      </div>
     </form>
      `
 }
